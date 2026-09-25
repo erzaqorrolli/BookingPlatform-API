4fe0992b8a6d240121df580a0b2e126a7649c06b
Erza Qorrolli <erzaqorrolli5@gmail.com>
2026-09-25 17:58:48 +0200

    changes

--- controllers/BookingController.php
+++ controllers/BookingController.php
@@ -362,7 +362,7 @@ class BookingController
         $notificationStmt = $db->prepare("
             SELECT
                 b.id,
-                b.reference
+                b.reference,
                 b.booking_date,
                 b.start_time,
                 b.total_price,
